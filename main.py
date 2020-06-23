44616ee5cd4fcb8e0df2f8f811e1623268a99036
bdoug <bdougla3@gmu.edu>
2020-06-23 14:04:58 -0400

    the new new

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from gtts import gTTS
 from playsound import playsound
+import os
+import requests
+import json
 
-tts = gTTS('Hello, my name is trey. Wubbalubbadubdub')
-tts.save('hello.mp3')
-playsound('hello.mp3')
 
+my_dictionary = {
+    'name': 'Trey',
+    'age': 27,
+    'friends': ['kyle', 'someone else?']
+}
+variable = my_dictionary['friends'][0]
+people = [my_dictionary, {
+    'stds': 'herps',
+}]
+print (people[1]['stds'])
+
+# token = os.environ['APITOKEN']
+# response = requests.get('https://graph.facebook.com/v7.0/me/feed?access_token=' + token)
+# jsondata = json.loads(response.text)
+# firtdictionary = jsondata['data']
+# first_array_value = firstdictionary[0]
+# message = first_array_value[]
+# tts = gTTS('Hello, my name is trey. Wubbalubbadubdub')
+# tts.save('hello.mp3')
+# playsound('hello.mp3')
