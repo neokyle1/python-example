c67e409191b12290ece2439e0ad7c9e09cecc6fd
kcunda200 <kyle_cundari@cable.comcast.com>
2020-06-23 17:30:10 -0400

    cool stuff

--- main.py
+++ main.py
@@ -16,21 +16,26 @@ variable = my_dictionary['friends'][0]
 people = [my_dictionary, {
     'stds': 'herps',
 }]
-print (people[1]['stds'])
+print(people[1]['stds'])
 
 # https://developers.facebook.com/tools/explorer/
 token = os.environ['APITOKEN']
-while True:
-    print('starting loop')
-    response = requests.get('https://graph.facebook.com/v7.0/me/feed?access_token=' + token)
-    if response.status_code != 200:
-        print('Boo!')
-        sys.exit(1)
-    jsondata = json.loads(response.text)
-    message = jsondata['data'][0]['message']
-    tts = gTTS(message)
-    tts.save('hello.mp3')
-    playsound('hello.mp3')
-    print('finishing loop')
-    time.sleep(2)
+
+# curl -X GET "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"
+
+# while True:
+print('starting loop')
+response = requests.get(f'https://graph.facebook.com/v7.0/me/feed?fields=from,message&access_token={token}')
+if response.status_code != 200:
+    print('Boo!')
+    sys.exit(1)
+jsondata = json.loads(response.text)
+from_name = jsondata['data'][0]['from']['name']
+message = jsondata['data'][0]['message']
+speech_message = f"Message from {from_name}, {message}"
+tts = gTTS(speech_message)
+tts.save('hello.mp3')
+playsound('hello.mp3')
+print('finishing loop')
+time.sleep(2)
 
